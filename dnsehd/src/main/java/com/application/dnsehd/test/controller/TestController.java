8a8c0265a06daed6ffdecc8009b8a0bce2deb725
yohan <yohan@dansaem>
2024-01-08 16:50:03 +0900

    20240108

--- dnsehd/src/main/java/com/application/dnsehd/test/controller/TestController.java
+++ dnsehd/src/main/java/com/application/dnsehd/test/controller/TestController.java
@@ -88,15 +88,16 @@ public class TestController {
 //		return "designTest/";
 //	}	
 
-
+	/* MemberController에 사용 완료 */
+	/*
 	@GetMapping("/register")
 	public String register() {
-		return "designTest/register";
+		return "member/register";
 	}	
 	
 	@GetMapping("/login")
 	public String login() {
-		return "designTest/login";
+		return "member/login";
 	}	
 
 	@GetMapping("/main")
@@ -159,7 +160,14 @@ public class TestController {
 		return "designTest/mypage";
 	}	
 	
-	// admin
+	@GetMapping("/addReview")
+	public String addReview() {
+		return "designTest/addReview";
+	}
+	
+	*/
+	
+	// 4. admin view와 맵핑
 	// ..을 고쳐서 사용
 //	@GetMapping("/ad..")
 //	public String  admin..() {
